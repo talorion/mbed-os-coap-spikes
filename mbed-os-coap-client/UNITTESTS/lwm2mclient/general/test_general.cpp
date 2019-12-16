4922c121e60291ae74bf5b2df864a76645457815
talorion <gregor.mayramhof@gmail.com>
2019-12-16 19:32:32 +0100

    minor tweaks

--- mbed-os-coap-client/UNITTESTS/lwm2mclient/general/test_general.cpp
+++ mbed-os-coap-client/UNITTESTS/lwm2mclient/general/test_general.cpp
@@ -1,6 +1,11 @@
 #include "CppUTest/TestHarness.h"
 #include "CppUTest/CommandLineTestRunner.h"
 
+#include<string>
+
+constexpr const char LWM2M_SERVER_ADDR[]="qa-iot.world-direct.at";
+constexpr uint16_t LWM2M_SERVER_PORT=5694;
+
 TEST_GROUP(Example)
 {
     void setup()
